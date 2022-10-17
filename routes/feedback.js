7a8121be785233c01dacfaac844a355b3115e5a2
Alex <alex@Alexs-MacBook-Air.local>
2022-10-18 00:19:18 +0100

    tailwind3 starter with express

---
+++ routes/feedback.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+let database_path = "database.db"
+
+const db = require('better-sqlite3')(database_path);
+
+/* GET feedbacks listing. */
+router.post('/', function(req, res, next) {
+    let createdAt = new Date();
+    db.prepare('INSERT INTO feedback(feedback_txt, created_at) values(?,?)')
+        .run(req.body.feedback_txt, createdAt.toISOString());
+    res.status(200).json({status:"ok"});
+});
+
+module.exports = router;
